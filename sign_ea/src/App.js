315a092aad776ae4ee10869c8e881f2b1f03ee19
Backendeng <backendeng0417@gmail.com>
2022-10-27 20:59:12 -0400

    complete 1

--- sign_ea/src/App.js
+++ sign_ea/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FirstPage from "./firstpage";
@@ -7,11 +8,23 @@ import ThirdPage from "./thirdpage";
 
 
 function App() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    console.log(email);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="second" element={<SecondPage />} />
+        <Route path="/" element={<FirstPage email={email} password={password} handleEmail={handleEmail} handlePassword={handlePassword} />} />
+        <Route path="second" element={<SecondPage email={email} password={password}/>}/>
         <Route path="third" element={<ThirdPage />} />
       </Routes>
     </BrowserRouter>
